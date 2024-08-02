2729ab93809c7639fb4e43805f79a3865f92c113
varun kangotra <varunkangotra.it25@gmail.com>
2024-08-02 11:26:18 +0530

    update past

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,29 +1,28 @@
+// route.ts Route Handlers
 import { Configuration, OpenAIApi } from "openai-edge";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
-export const runtime = 'edge';
+export const runtime = 'edge'; // Provide optimal infrastructure for our API route (https://edge-runtime.vercel.app/)
 
 const config = new Configuration({
-    apiKey: "sk-proj-YA1Ug2l7VPWyhPmVCsNPT3BlbkFJOfUQQYe7D4ekprwmibTT",
-});
+    apiKey: process.env.OPENAI_API_KEY
+})
 
 const openai = new OpenAIApi(config);
 
+
+// POST localhost:3000/api/chat
 export async function POST(request: Request) {
-    try {
-        const { messages } = await request.json();
+    const { messages } = await request.json();
+        
+    const response = await openai.createChatCompletion({
+        model: 'gpt-4',
+        stream: true,
+        messages: [...messages]
+    })
+    
+    const stream = await OpenAIStream(response);
 
-        const response = await openai.createChatCompletion({
-            model: 'gpt-4',
-            stream: true,
-            messages: [...messages],
-        });
-
-        const stream = await OpenAIStream(response);
-
-        return new StreamingTextResponse(stream);
-    } catch (error) {
-        console.error('Error in POST /api/chat:', error);
-        return new Response(JSON.stringify({ error: 'Internal Server Error' }), { status: 500 });
-    }
+    // send the stream as a response to our client / frontend
+    return new StreamingTextResponse(stream);
 }
